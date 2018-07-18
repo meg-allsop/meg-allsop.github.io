2a87ca9bb2357b7fd7cad10a183c0876662c16e1
megan-allsop <38864406+megan-allsop@users.noreply.github.com>
2018-07-18 10:56:18 -0600

    json

--- acme/js/acme.js
+++ acme/js/acme.js
@@ -1,8 +1,10 @@
 console.log("connected");
 
 const URL = "/acme/js/acme.json";
-fetch(URL).then(Response => acme.json());
+fetch(URL)
+  .then(Response => acme.json())
+  .then(function (data)) {
+  console.log("Json object is put in nav bar:")
+  console.log(data)
 
-{
-  console.log("Json object is put in nav bar:");
-}
+}); 
